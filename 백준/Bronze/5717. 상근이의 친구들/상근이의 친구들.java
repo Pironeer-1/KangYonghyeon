44cb3be47db7686e56f2fdab3fd67f6a517b7a0d
khyeon1003 <154689763+khyeon1003@users.noreply.github.com>
2024-03-22 16:24:22 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 208 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    while (true) {
+      int male=input.nextInt();
+      int female=input.nextInt();
+      if (male ==0 && female==0) {
+        break;
+      }else{
+        int friends=male+female;
+        System.out.println(friends);
+      }
+    }
+  }
+}
