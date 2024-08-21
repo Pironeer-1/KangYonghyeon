fccc75bab16003e55efc5ae03ba8de002d311586
khyeon1003 <154689763+khyeon1003@users.noreply.github.com>
2024-08-21 12:11:42 +0900

    [Bronze II] Title: 수 정렬하기, Time: 276 ms, Memory: 22228 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-  
   public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
     int N= sc.nextInt();
@@ -9,17 +8,23 @@ public class Main {
     for (int i=0;i<N;i++){
       arr[i]=sc.nextInt();
     }
-    for (int i=0 ; i<arr.length-1;i++){
-      for (int j=0; j<arr.length-1-i;j++){
-        if (arr[j]>arr[j+1]){
-          int temp=arr[j];
-          arr[j]=arr[j+1];
-          arr[j+1]=temp;
+    //arr를 완성
+    for (int i=0; i<arr.length-1;i++){
+      int minIndex=i;//제일 처음 값을 minIndex로 잡고 
+      for(int j=i+1; j<arr.length;j++){
+        if(arr[j]<arr[minIndex]){
+          minIndex=j;
         }
       }
+
+      int temp= arr[i];
+      arr[i]=arr[minIndex];
+      arr[minIndex]=temp;
     }
+      //정렬 완료
     for (int i=0; i< arr.length; i++){
       System.out.println(arr[i]);
     }
   }
+
 }
