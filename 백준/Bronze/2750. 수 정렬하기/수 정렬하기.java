f7e8f883a4746393d9822246c0efb09023e5c2f7
khyeon1003 <154689763+khyeon1003@users.noreply.github.com>
2024-08-21 14:17:42 +0900

    [Bronze II] Title: 수 정렬하기, Time: 272 ms, Memory: 22344 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,30 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
+  public static int [] insertion_sort(int[] arr){
+    //매개변수도 똑같이 타입 배열 변수명
+    for (int i=1; i<arr.length;i++){
+      int j;
+      int insertingData=arr[i];
+      for (j=i-1;j>=0;j--){
+        if(arr[j]>insertingData){
+          arr[j+1]=arr[j];
+        }else{
+          //삽입할 원소의 자리를 찾았어-> j+1이지
+          break;
+        }
+      }
+      arr[j+1]=insertingData;
+      //여기서 j를 다시 사용하기 위해서 for 문밖에서 선언
+    }
+    return arr;
+  }
   public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
+    //사이즈 선언하고 for문으로 데이터 받기
     int N= sc.nextInt();
     int[] arr=new int[N];
     for (int i=0;i<N;i++){
       arr[i]=sc.nextInt();
     }
     //arr를 완성
-    for (int i=0; i<arr.length-1;i++){
-      int minIndex=i;//제일 처음 값을 minIndex로 잡고 
-      for(int j=i+1; j<arr.length;j++){
-        if(arr[j]<arr[minIndex]){
-          minIndex=j;
-        }
-      }
+    //삽입 정렬
+    insertion_sort(arr);
 
-      int temp= arr[i];
-      arr[i]=arr[minIndex];
-      arr[minIndex]=temp;
-    }
-      //정렬 완료
+    //정렬 완료
     for (int i=0; i< arr.length; i++){
       System.out.println(arr[i]);
     }
+    sc.close();
   }
-
 }
